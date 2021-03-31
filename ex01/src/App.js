c499895b5f3c8efc2fd2b82d287cdd52fe42df6c
basslines <admir.osmanovic@gmail.com>
2021-03-31 14:24:55 +0200

    dodao ex01 projekat

---
+++ ex01/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.setMyStorage}>Set my Storage</button>
+      </div>
+    );
+  }
+  setMyStorage = () => {
+    document.cookie = "Year=2021";
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+  };
+}
+export default App;
